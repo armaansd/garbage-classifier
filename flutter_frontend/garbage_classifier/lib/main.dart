8cd6fb569ac2163a35e17f13383a88260cc64549
yecs1999 <chrischromeacc@gmail.com>
2021-01-30 13:18:54 -0800

    first vers of main page

--- flutter_frontend/garbage_classifier/lib/main.dart
+++ flutter_frontend/garbage_classifier/lib/main.dart
@@ -1,36 +1,104 @@
 import 'dart:async';
 import 'dart:io';
 import 'package:flutter/material.dart';
+//import 'package:firebase_core/firebase_core.dart';
 import 'package:image_picker/image_picker.dart';
 
 
-void main() => runApp(new MyApp ());
+void main() {
+  //WidgetsFlutterBinding.ensureInitialized();
+  runApp(MyApp());
+}
 
 class MyApp extends StatefulWidget{
   @override
-  State<StatefulWidget> createState() => new _MyAppState ();
+  State<StatefulWidget> createState() =>  _MyAppState ();
 
 }
 
 class _MyAppState extends State<MyApp> {
-  File _image;
+  // Set default `_initialized` and `_error` state to false
+  /*
+  bool _initialized = false;
+  bool _error = false;
 
-  Future getImage() async {
-    var image  = await ImagePicker.pickImage(source: ImageSource.camera);
-    setState(() {
-      _image = image;
-    });
+  // Define an async function to initialize FlutterFire
+
+  void initializeFlutterFire() async {
+    try {
+      // Wait for Firebase to initialize and set `_initialized` state to true
+      await Firebase.initializeApp();
+      setState(() {
+        _initialized = true;
+      });
+    } catch(e) {
+      // Set `_error` state to true if Firebase initialization fails
+      setState(() {
+        _error = true;
+      });
+    }
   }
 
   @override
+  void initState() {
+    initializeFlutterFire();
+    super.initState();
+  }
+
+   */
+  @override
   Widget build(BuildContext context) {
-  return new MaterialApp(title: 'Image Picker',
-  home: new Scaffold(
-    appBar: new AppBar(title: new Text('Image Picker'),
+    // Show error message if initialization failed
+    /*
+    if(_error) {
+      return MaterialApp(
+        home: Scaffold(
+          body: Text("Firebase failed to load.")
+        )
+      );
+    }
+
+    // Show a loader until FlutterFire is initialized
+    if (!_initialized) {
+      return MaterialApp(
+          home: Scaffold(
+              body: Text("Loading... Please be patient :)")
+          )
+      );
+    }
+
+     */
+    return MaterialApp(theme:
+        ThemeData(brightness: Brightness.dark,
+                  primaryColor:Colors.blueGrey),
+      home: Scaffold(
+        appBar:  AppBar(title: Text('Garbify', textAlign: TextAlign.center,),
     ),
-    body: new Center(child: _image == null? new Text('No image selected'):new Image.file(_image),
+    body: Column(
+      children: <Widget> [Container(
+        margin:  EdgeInsets.only(top:50),
+        child: Center(
+          child: Image(image: AssetImage('test_imgs/profile.png'))
+        )
+      ),
+      Container(
+        margin: EdgeInsets.only(top:10),
+        child: Center(
+          child: Text('chrisye1',textAlign:TextAlign.center,),
+        )
+      ),
+        Container(
+            margin: EdgeInsets.only(top:10),
+            child: Center(
+              child: Text('5000',textAlign:TextAlign.center,),
+            )
+        ),
+      ]
+
     ),
-    floatingActionButton: new FloatingActionButton(onPressed: getImage,tooltip: 'Pick Image',child: new Icon(Icons.camera) ,),
+
+    //floatingActionButton: new FloatingActionButton(onPressed: getImage,tooltip: 'Pick Image',child: new Icon(Icons.camera) ,),
+        floatingActionButton: new FloatingActionButton(tooltip: 'Pick Image',child: new Icon(Icons.camera) ,),
   ),);
   }
 
